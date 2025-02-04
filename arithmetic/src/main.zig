ab750170051de2c18e1902e6e4db1d711a7b7a97
Michael Torres <mrmojorisen0@gmail.com>
2025-02-03 23:50:22 -0500

    Ok, cool. I am done with the first assignment/practice.

--- arithmetic/src/main.zig
+++ arithmetic/src/main.zig
@@ -8,7 +8,37 @@ pub fn main() !void {
         \\                                          Welcome
         \\------------------------------------------------------------------------------------------
         \\
+        \\
     );
+
+    const x = 5;
+    const y = 2;
+    var result: i8 = undefined;
+
+    try stdout.print("Adding {} to {}\n", .{ x, y });
+    result = add(x, y);
+    try stdout.print("result = {}\n\n", .{result});
+
+    try stdout.print("Subtracting {} from {}\n", .{ y, x });
+    result = subtract(x, y);
+    try stdout.print("result = {}\n\n", .{result});
+
+    try stdout.print("Multiplying {} by {}\n", .{ x, y });
+    result = multiply(x, y);
+    try stdout.print("result = {}\n\n", .{result});
+
+    try stdout.print("Dividing {} by {}\n", .{ x, y });
+    result = divide(x, y);
+    try stdout.print("result = {}\n\n", .{result});
+
+    try stdout.print("Getting the remainder of the division of {} by {}\n", .{ x, y });
+    result = modulus(x, y);
+    try stdout.print("result = {}\n\n", .{result});
+
+    try stdout.print("Raising {} by the power of {}\n", .{ x, y });
+    result = power(x, y);
+    try stdout.print("result = {}\n\n", .{result});
+
     _ = try stdout.write(
         \\------------------------------------------------------------------------------------------
         \\                                          Goodbye 
@@ -42,4 +72,49 @@ test add {
 
 fn add(x: i8, y: i8) i8 {
     return x + y;
+}
+
+// -------------------------------------------------------------------------------------------------
+test subtract {
+    try std.testing.expect(subtract(100, 99) == 1);
+}
+
+fn subtract(x: i8, y: i8) i8 {
+    return x - y;
+}
+
+// -------------------------------------------------------------------------------------------------
+test multiply {
+    try std.testing.expect(multiply(50, 2) == 100);
+}
+
+fn multiply(x: i8, y: i8) i8 {
+    return x * y;
+}
+
+// -------------------------------------------------------------------------------------------------
+test divide {
+    try std.testing.expect(divide(5, 2) == 2);
+}
+
+fn divide(x: i8, y: i8) i8 {
+    return @divTrunc(x, y);
+}
+
+// -------------------------------------------------------------------------------------------------
+test modulus {
+    try std.testing.expect(modulus(5, 2) == 1);
+}
+
+fn modulus(x: i8, y: i8) i8 {
+    return @mod(x, y);
+}
+
+// -------------------------------------------------------------------------------------------------
+test power {
+    try std.testing.expect(power(5, 2) == 25);
+}
+
+fn power(x: i8, y: i8) i8 {
+    return std.math.pow(i8, x, y);
 }
