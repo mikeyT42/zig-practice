ff42aace68d99a73566a8c9e78bd9a7c17a92517
Michael Torres <mrmojorisen0@gmail.com>
2025-02-11 09:33:03 -0500

    Getting change-counter setup.

--- change-counter/src/main.zig
+++ change-counter/src/main.zig
@@ -1,3 +1,66 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
-pub fn main() !void {}
+const LoopControl = enum {
+    again,
+    stop,
+};
+
+pub fn main() !void {
+    try clear();
+    _ = try stdout.write(
+        \\------------------------------------------------------------------------------------------
+        \\                                          Welcome
+        \\------------------------------------------------------------------------------------------
+        \\
+        \\
+    );
+
+    var loop_control = LoopControl.again;
+    while (loop_control == LoopControl.again) {
+        loop_control = try input_loop();
+    }
+
+    _ = try stdout.write(
+        \\
+        \\------------------------------------------------------------------------------------------
+        \\                                          Goodbye 
+        \\------------------------------------------------------------------------------------------
+        \\
+    );
+}
+
+// -------------------------------------------------------------------------------------------------
+fn clear() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    const result = try std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = &[_][]const u8{"clear"},
+    });
+    defer {
+        allocator.free(result.stdout);
+        allocator.free(result.stderr);
+    }
+
+    _ = try stdout.write(result.stdout);
+}
+
+// -------------------------------------------------------------------------------------------------
+fn input_loop() !LoopControl {
+    const buf_size = comptime 250;
+    const sentinel = comptime '\n';
+    var input_buf: [buf_size]u8 = undefined;
+
+    _ = try stdout.write(
+        \\Enter the amount you spent to two decimal places: the input must be between 0 and 1: -1 is
+        \\to exit.
+        \\
+    );
+    const line = try stdin.readUntilDelimiterOrEof(&input_buf, sentinel);
+    if (line.?.len == 0)
+        return LoopControl.stop;
+}
