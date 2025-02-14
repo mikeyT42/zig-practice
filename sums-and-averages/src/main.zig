ddde76141494f60805313952396ff1e05f37b818
mikeyT42 <mrmojorisen0@gmail.com>
2025-02-14 01:48:22 -0500

    I added structs to represent the averages, counts, and sums.

--- sums-and-averages/src/main.zig
+++ sums-and-averages/src/main.zig
@@ -1,3 +1,21 @@
 const std = @import("std");
 
+const Sums = struct {
+    positive: f32,
+    negatice: f32,
+    overall: f32,
+};
+
+const Counts = struct {
+    positive: u16,
+    negative: u16,
+    overall: u16,
+};
+
+const Averages = struct {
+    positive: f32,
+    negative: f32,
+    overall: f32,
+};
+
 pub fn main() !void {}
