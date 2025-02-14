cc3daccd17b77120890ef13ffd7471713915085d
mikeyT42 <mrmojorisen0@gmail.com>
2025-02-14 13:26:58 -0500

    I am getting closer with the program.

--- sums-and-averages/src/main.zig
+++ sums-and-averages/src/main.zig
@@ -2,25 +2,32 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
 
+const max_items = 10;
+
 const Sums = struct {
-    positive: f32,
-    negatice: f32,
-    overall: f32,
+    positive: f32 = 0.0,
+    negatice: f32 = 0.0,
+    overall: f32 = 0.0,
 };
 
 const Counts = struct {
-    positive: u16,
-    negative: u16,
-    overall: u16,
+    positive: u16 = 0,
+    negative: u16 = 0,
+    overall: u16 = 0,
 };
 
 const Averages = struct {
-    positive: f32,
-    negative: f32,
-    overall: f32,
+    positive: f32 = 0.0,
+    negative: f32 = 0.0,
+    overall: f32 = 0.0,
 };
 
-const InputValidation = union(enum) {};
+const InputValidation = union(enum) {
+    ok: []f16,
+    no_input: void,
+    too_many: void,
+    input_error: std.fmt.ParseFloatError,
+};
 
 const LoopControl = enum {
     again,
@@ -73,7 +80,6 @@ fn clear() !void {
 fn input_loop() !LoopControl {
     const buf_size = comptime 250;
     const sentinel = comptime '\n';
-    const len_of_items = comptime 10;
 
     var input_buf: [buf_size]u8 = undefined;
 
@@ -82,15 +88,84 @@ fn input_loop() !LoopControl {
         \\Simply enter a newline character to exit.
         \\
         \\
-        , .{len_of_items});
+    , .{max_items});
     const line = try stdin.readUntilDelimiterOrEof(&input_buf, sentinel);
-    if (line.?.len <= 0)
-        return LoopControl.stop;
 
-    const input = line.?;
+    const numbers = switch(validate(line)) {
+        InputValidation.no_input => {
+            _ = try stdout.write("Sorry, but you didn't enter any input. Please try again.\n\n");
+            return LoopControl.again;
+        },
+        InputValidation.too_many => {
+            _ = try stdout.print("You input too many numbers, please only input up to {d}.\n\n", .{max_items});
+            return LoopControl.again;
+        },
+        InputValidation.input_error => |err| {
+            _ = try stdout.print("You did not input valid input [{s}]\nerror:\n{}\n\n", .{ line.?, err });
+            return LoopControl.again;
+        },
+        InputValidation.ok => |parsed_numbers| parsed_numbers,
+    };
+
+    var sums: Sums = .{};
+    var counts: Counts = .{};
+    var averages: Averages = .{};
 
     return LoopControl.again;
 }
 
 // -------------------------------------------------------------------------------------------------
-fn validate(input: ?[]const u8) InputValidation {}
+fn validate(optional_input: ?[]const u8) InputValidation {
+    const input: []const u8 = optional_input orelse return InputValidation.no_input;
+    if (input.len == 0)
+        return InputValidation.no_input;
+    var nums = std.mem.splitScalar(u8, input, ' ');
+    if (nums.peek() == null)
+        return InputValidation.no_input;
+
+    var parsed_nums: [max_items]f16 = undefined;
+    for (nums.next(), 0..) |num, i| {
+        if (i == max_items)
+            return InputValidation.too_many;
+
+        parsed_nums[i] = std.fmt.parseFloat(f16, num) catch |err| return InputValidation{ .input_error = err };
+    }
+
+    return InputValidation{ .ok = parsed_nums };
+}
+
+// -------------------------------------------------------------------------------------------------
+fn sum_and_count(numbers: []const f16, sums: *Sums, counts: *Counts) void {
+    for (numbers) |number| {
+        if (number >= 0) {
+            sums.*.positive += number;
+            counts.*.positive += 1;
+        } else {
+            sums.*.negative += number;
+            counts.*.negative += 1;
+        }
+        sums.*.overall += number;
+        counts.*.overall += 1;
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+fn average(sums: *const Sums, counts: *const Counts, averages: *Averages) void {
+    if(counts.*.positive == 0) {
+        averages.*.positive = 0;
+    } else {
+        averages.*.positive = sums.*.positive / counts.*.positive;
+    }
+
+    if (counts.*.negative == 0) {
+        averages.*.negative = 0;
+    } else {
+        averages.*.negative = sums.*.negative / counts.*.negative;
+    }
+
+    if (sums.*.overall == 0 or counts.*.overall == 0) {
+        averages.*.overall = 0;
+    } else {
+        averages.*.overall = sums.*.overall / counts.*.overall;
+    }
+}
