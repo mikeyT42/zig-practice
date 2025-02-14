2455b21009e2cea68600c71e78060bd7344975da
mikeyT42 <mrmojorisen0@gmail.com>
2025-02-14 01:52:07 -0500

    I have to commit now because I am shot.

--- sums-and-averages/src/main.zig
+++ sums-and-averages/src/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
 const Sums = struct {
     positive: f32,
@@ -18,4 +20,66 @@ const Averages = struct {
     overall: f32,
 };
 
-pub fn main() !void {}
+const LoopControl = enum {
+    again,
+    stop,
+};
+
+pub fn main() !void {
+    try clear();
+    _ = try stdout.write(
+        \\------------------------------------------------------------------------------------------
+        \\                                          Welcome
+        \\------------------------------------------------------------------------------------------
+        \\
+        \\
+    );
+
+    var loop_control = LoopControl.again;
+    while (loop_control == LoopControl.again) {
+        loop_control = try input_loop();
+    }
+
+    _ = try stdout.write(
+        \\
+        \\------------------------------------------------------------------------------------------
+        \\                                          Goodbye 
+        \\------------------------------------------------------------------------------------------
+        \\
+    );
+}
+
+// -------------------------------------------------------------------------------------------------
+fn clear() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    const result = try std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = &[_][]const u8{"clear"},
+    });
+    defer {
+        allocator.free(result.stdout);
+        allocator.free(result.stderr);
+    }
+
+    _ = try stdout.write(result.stdout);
+}
+
+// -------------------------------------------------------------------------------------------------
+fn input_loop() !LoopControl {
+    const buf_size = comptime 250;
+    const sentinel = comptime '\n';
+
+    var input_buf: [buf_size]u8 = undefined;
+
+    _ = try stdout.write("\n\nPlease input a sentence. If you want to exit, just hit enter.\n");
+    const line = try stdin.readUntilDelimiterOrEof(&input_buf, sentinel);
+    if (line.?.len <= 0)
+        return LoopControl.stop;
+
+    const input = line.?;
+
+    return LoopControl.again;
+}
