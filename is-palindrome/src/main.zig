d726ef684d834535259a8eeb2ac98408fc17fc8c
Michael Torres <mrmojorisen0@gmail.com>
2025-02-19 14:13:29 -0500

    Some more setup.

--- is-palindrome/src/main.zig
+++ is-palindrome/src/main.zig
@@ -1,3 +1,50 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
-pub fn main() !void {}
+const LoopControl = enum {
+    again,
+    stop,
+};
+
+pub fn main() !void {
+    try clear();
+    _ = try stdout.write(
+        \\------------------------------------------------------------------------------------------
+        \\                          Welcome to the Palindrome Checker
+        \\------------------------------------------------------------------------------------------
+        \\
+        \\
+    );
+
+    var loop_control = LoopControl.again;
+    while (loop_control == LoopControl.again) {
+        loop_control = try input_loop();
+    }
+
+    _ = try stdout.write(
+        \\
+        \\------------------------------------------------------------------------------------------
+        \\                              Dhank you, come again. 
+        \\------------------------------------------------------------------------------------------
+        \\
+    );
+}
+
+// -------------------------------------------------------------------------------------------------
+fn clear() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    const result = try std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = &[_][]const u8{"clear"},
+    });
+    defer {
+        allocator.free(result.stdout);
+        allocator.free(result.stderr);
+    }
+
+    _ = try stdout.write(result.stdout);
+}
