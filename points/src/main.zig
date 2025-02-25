ccabe486aa6dbf9c47f7269f9055678e89837672
Michael Torres <mrmojorisen0@gmail.com>
2025-02-24 22:50:53 -0500

    Getting the basics in.

--- points/src/main.zig
+++ points/src/main.zig
@@ -1,3 +1,62 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
-pub fn main() !void {}
+const LoopControl = enum {
+    again,
+    stop,
+};
+
+pub fn main() !void {
+    try clear();
+    _ = try stdout.write(
+        \\------------------------------------------------------------------------------------------
+        \\                          Welcome to the Palindrome Checker
+        \\------------------------------------------------------------------------------------------
+        \\
+        \\
+    );
+
+    var loop_control = LoopControl.again;
+    while (loop_control == LoopControl.again) {
+        loop_control = try inputLoop();
+    }
+
+    _ = try stdout.write(
+        \\
+        \\------------------------------------------------------------------------------------------
+        \\                              Dhank you, come again. 
+        \\------------------------------------------------------------------------------------------
+        \\
+    );
+}
+
+// -------------------------------------------------------------------------------------------------
+fn clear() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    const result = try std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = &[_][]const u8{"clear"},
+    });
+    defer {
+        allocator.free(result.stdout);
+        allocator.free(result.stderr);
+    }
+
+    _ = try stdout.write(result.stdout);
+}
+
+// -------------------------------------------------------------------------------------------------
+fn inputLoop() !LoopControl {
+    const buf_len: u8 = comptime 32;
+    const sentinel: u8 = comptime '\n';
+
+    _ = try stdout.write("Please input 2 integers, an x and y value, for a point in space.\n");
+
+    var input_buf: [buf_len]u8 = undefined;
+    const line = try stdin.readUntilDelimiterOrEof(&input_buf, sentinel);
+    _ = line orelse "";
+}
